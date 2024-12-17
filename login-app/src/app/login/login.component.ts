981d5d383f3a067bc7738c9256a645b61875807e
dorianguido <149299389+dorianguido@users.noreply.github.com>
2024-12-17 20:00:31 +0100

    create login app

---
+++ login-app/src/app/login/login.component.ts
@@ -0,0 +1,35 @@
+//import { Component } from '@angular/core';
+
+//@Component({
+  //selector: 'app-login',
+  //templateUrl: './login.component.html',
+//  styleUrl: './login.component.scss'
+//})
+// export class LoginComponent {
+
+//}
+//
+
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+ 
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+ 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      username: '',
+      password: ''
+    });
+  }
+ 
+  onSubmit() {
+    // Handle login submission
+    console.log(this.loginForm.value);
+  }
+}
